95b56732df59eeab7a4c6f52d96c439c0c41c17c
Essence Alexander <upperlefty@zoho.eu>
2018-03-22 15:43:02 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import Tickers from './components/Tickers.js';
+ import React, { Component } from 'react';
+    import './App.css';
+
+    class App extends Component {
+        render() {
+            return (
+                <div className="App">
+                    <div className="App-header">
+                        <h2>Cryptocurrency Ticker</h2>
+                    </div>
+                    <Tickers />
+                </div>
+            );
+        }
+    }
+
+    export default App;
